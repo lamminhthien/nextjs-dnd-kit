01a9d79746aebdb16218d32ee18658615b71ee56
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-10-25 09:56:06 +0700

    init

---
+++ pages/mc/prop.type.ts
@@ -0,0 +1,32 @@
+import {CancelDrop, KeyboardCoordinateGetter, UniqueIdentifier, Modifiers} from '@dnd-kit/core';
+import {SortingStrategy} from '@dnd-kit/sortable';
+
+type Items = Record<UniqueIdentifier, UniqueIdentifier[]>;
+
+export interface IPropsMultiContainer {
+  adjustScale?: boolean;
+  cancelDrop?: CancelDrop;
+  columns?: number;
+  containerStyle?: React.CSSProperties;
+  coordinateGetter?: KeyboardCoordinateGetter;
+  getItemStyles?(args: {
+    value: UniqueIdentifier;
+    index: number;
+    overIndex: number;
+    isDragging: boolean;
+    containerId: UniqueIdentifier;
+    isSorting: boolean;
+    isDragOverlay: boolean;
+  }): React.CSSProperties;
+  wrapperStyle?(args: {index: number}): React.CSSProperties;
+  itemCount?: number;
+  items?: Items;
+  handle?: boolean;
+  renderItem?: any;
+  strategy?: SortingStrategy;
+  modifiers?: Modifiers;
+  minimal?: boolean;
+  trashable?: boolean;
+  scrollable?: boolean;
+  vertical?: boolean;
+}
